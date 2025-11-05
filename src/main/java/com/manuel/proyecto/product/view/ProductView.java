c6dfa2c0e46562dd77af64bf28c141a2f9bb67ee
mmbalbas <mmbalbasgolf@gmail.com>
2025-11-05 11:10:11 +0100

    Add methods for displaying, updating, and deleting products in ProductView

--- src/main/java/com/manuel/proyecto/product/view/ProductView.java
+++ src/main/java/com/manuel/proyecto/product/view/ProductView.java
@@ -5,9 +5,9 @@ import com.manuel.proyecto.product.exceptions.InvalidProductException;
 import com.manuel.proyecto.product.model.Product;
 import com.manuel.proyecto.product.model.ProductCategory;
 
+import java.util.Optional;
 import java.util.Scanner;
 
-import static com.sun.beans.introspect.PropertyInfo.Name.description;
 
 public class ProductView {
     private final ProductController productController;
@@ -18,6 +18,118 @@ public class ProductView {
         this.productController = productController;
         scanner = new Scanner(System.in);
 
+    }
+
+    private void showAllProducts() {
+        try {
+            System.out.println("\nLista de Productos:");
+            for (Product product : productController.getAllProducts()) {
+                System.out.println(product);
+            }
+        } catch (InvalidProductException e) {
+            System.out.println("Error al obtener los productos: " + e.getMessage());
+        }
+    }
+
+    private void showProduct(Product product1){
+        try {
+            long id = readValidLong("Ingrese el ID del producto a mostrar (mayor o igual a 0):", 0);
+            Product product = productController.getProductById(id).orElse(null);
+            if (product != null) {
+                System.out.println("Detalles del Producto:");
+                System.out.println(product);
+            } else {
+                System.out.println("Producto no encontrado con ID: " + id);
+            }
+        } catch (InvalidProductException e) {
+            System.out.println("Error al obtener el producto: " + e.getMessage());
+        }
+    }
+
+    private void findProductById() {
+        try {
+            long id = readValidLong("Ingrese el ID del producto a buscar (mayor o igual a 0):", 0);
+            Optional<Product> product = productController.getProductById(id);
+            if (product.isPresent()) {
+                Product product1 = product.get();
+                showProduct(product1);
+            } else {
+                System.out.println("Producto no encontrado con ID: " + id);
+            }
+        } catch (InvalidProductException e) {
+            System.out.println("Error al buscar el producto: " + e.getMessage());
+        }
+    }
+
+    private void eliminarProduct() {
+        try {
+            long id = readValidLong("Ingrese el ID del producto a eliminar (mayor o igual a 0):", 0);
+            productController.removeProduct(id);
+            System.out.println("Producto eliminado exitosamente.");
+        } catch (InvalidProductException e) {
+            System.out.println("Error al eliminar el producto: " + e.getMessage());
+        }
+    }
+
+    private void updateProduct() {
+        try {
+            long id = readValidLong("Ingrese el ID del producto a modificar (mayor o igual a 0):", 0);
+          Optional<Product> product = productController.getProductById(id);
+            if (product.isPresent()) {
+                System.out.println("Producto a ACTUALIZAR");
+                    Product product1 = product.get();
+                    showProduct(product1);
+                System.out.println("Seleccione el campo a modificar:");
+                System.out.println("1. Nombre");
+                System.out.println("2. Precio");
+                System.out.println("3. Categoría");
+                System.out.println("4. Stock");
+                System.out.println("5. TODOS LOS CAMPOS");
+                System.out.println("6. Salir sin modificar");
+                 int option = scanner.nextInt();
+                scanner.nextLine();
+
+                switch (option) {
+                    case 1 -> {
+                        String nombre = readNotEmptyString("Ingrese el nuevo nombre del producto:");
+                        product1.setNombre(nombre);
+                    }
+                    case 2 -> {
+                        double precio = readValidDouble("Ingrese el nuevo precio del producto (mayor o igual a 0):", 5);
+                        product1.setPrecio(precio);
+                    }
+                    case 3 -> {
+                        String categoria = readNotEmptyString("Ingrese la nueva categoría del producto: \nELECTRONICS, COMIDAS, LIBROS, OTROS");
+                        product1.setCategoria(ProductCategory.valueOf(categoria.toUpperCase()));
+                    }
+                    case 4 -> {
+                        int stock = readValidInteger("Ingrese el nuevo stock del producto (mayor o igual a 0):", 2);
+                        product1.setStock(stock);
+                    }
+                    case 5 -> {
+                        String nombre = readNotEmptyString("Ingrese el nuevo nombre del producto:");
+                        double precio = readValidDouble("Ingrese el nuevo precio del producto (mayor o igual a 0):", 5);
+                        int stock = readValidInteger("Ingrese el nuevo stock del producto (mayor o igual a 0):", 2);
+                        String categoria = readNotEmptyString("Ingrese la nueva categoría del producto: \nELECTRONICS, COMIDAS, LIBROS, OTROS");
+
+                        product1.setNombre(nombre);
+                        product1.setPrecio(precio);
+                        product1.setStock(stock);
+                        product1.setCategoria(ProductCategory.valueOf(categoria.toUpperCase()));
+                    }
+                    case 6 -> {
+                        System.out.println("Saliendo sin modificar el producto.");
+                        return;
+                    }
+                    default -> {
+                        System.out.println("Opción no válida. Saliendo sin modificar el producto.");
+                        return;
+                    }
+                }
+            }
+        } catch (InvalidProductException e) {
+            System.out.println("Error al modificar el producto: " + e.getMessage());
+        }
     }
 
     public void showMenu() {
@@ -34,9 +146,18 @@ public class ProductView {
             scanner.nextLine();
 
             switch (option) {
-                case 1 -> {
-                return;
-                }
+                    case 1 -> addProduct();
+                    case 2 -> showAllProducts();
+                    case 3 -> findProductById();
+                    case 4 -> eliminarProduct();
+                    case 5 -> updateProduct();
+                    case 6 -> {
+                        System.out.println("Saliendo del menu de productos.");
+                        scanner.close();
+                        return;
+                    }
+
+
             }
         }
     }
@@ -56,6 +177,10 @@ public class ProductView {
             System.out.println("Error al agregar el producto: " + e.getMessage());
         }
     }
+
+
+
+
     private String readNotEmptyString(String message) {
         String input;
         do {
